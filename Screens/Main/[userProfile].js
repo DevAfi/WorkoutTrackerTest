d4cf1564258994b6ff969ceff6e8335dbd6e7911
Afonso Carvalho <afonsomiguelcarvalho2006@gmail.com>
2025-07-30 17:37:08 -0400

    Added user banner to profile

--- Screens/Main/[userProfile].js
+++ Screens/Main/[userProfile].js
@@ -1,5 +1,5 @@
 import React, { useLayoutEffect } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
 
 const UserProfile = ({ route, navigation }) => {
   const { username, full_name, goal } = route.params;
@@ -11,12 +11,20 @@ const UserProfile = ({ route, navigation }) => {
 
   return (
     <View style={styles.container}>
-      <View style={styles.topSectionContainer}>
-        <Text style={{ fontSize: 24, color: "white" }}>{full_name}</Text>
-      </View>
-
-      <Text style={{ fontSize: 18, color: "white" }}>@{username}</Text>
-      <Text style={{ fontSize: 16, color: "white" }}>Goal: {goal}</Text>
+      <ScrollView style={{ width: "100%", height: "100%" }}>
+        <View style={styles.topSectionContainer}>
+          <View style={styles.pfp}>
+            <Text style={{ fontSize: 24, fontWeight: "bold" }}>
+              {full_name[0]}
+            </Text>
+          </View>
+          <View style={styles.topSectionText}>
+            <Text style={{ fontSize: 24, color: "white" }}>@{username}</Text>
+            <Text style={{ fontSize: 12, color: "grey" }}>{full_name}</Text>
+            <Text style={{ fontSize: 12, color: "grey" }}>Goal: {goal}</Text>
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 };
@@ -27,10 +35,26 @@ const styles = StyleSheet.create({
     backgroundColor: "#252323",
     alignItems: "center",
     flexDirection: "column",
+    paddingVertical: 10,
+    paddingHorizontal: 15,
   },
   topSectionContainer: {
     backgroundColor: "#3C3939",
-    width: "90%",
+    width: "100%",
+    alignItems: "center",
+    flexDirection: "row",
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    borderRadius: 10,
+    gap: 20,
+  },
+  pfp: {
+    backgroundColor: "pink",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 100,
+    width: 50,
+    height: 50,
   },
 });
 export default UserProfile;
