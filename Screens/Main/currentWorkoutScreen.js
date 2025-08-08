36ef9a35d000d9911862682cbfb6dc75babd62f8
Afonso Carvalho <afonsomiguelcarvalho2006@gmail.com>
2025-08-07 20:23:26 -0400

    Started styling modal

--- Screens/Main/currentWorkoutScreen.js
+++ Screens/Main/currentWorkoutScreen.js
@@ -62,7 +62,6 @@ const CurrentWorkoutScreen = () => {
 
       <Modal
         animationType="slide"
-        transparent={true}
         visible={modalVisible}
         onRequestClose={() => {
           Alert.alert("Modal has been closed.");
@@ -76,7 +75,7 @@ const CurrentWorkoutScreen = () => {
               style={[styles.button, styles.buttonClose]}
               onPress={() => setModalVisible(!modalVisible)}
             >
-              <Text style={styles.textStyle}>Hide Modal</Text>
+              <Text style={styles.textStyle}>Hide Notes</Text>
             </TouchableOpacity>
           </View>
         </View>
@@ -98,7 +97,7 @@ const CurrentWorkoutScreen = () => {
           style={[styles.button, styles.buttonOpen]}
           onPress={() => setModalVisible(true)}
         >
-          <Text style={styles.textStyle}>Show Modal</Text>
+          <Text style={styles.textStyle}>Show Notes</Text>
         </TouchableOpacity>
 
         {sessionId ? (
@@ -155,6 +154,46 @@ const styles = StyleSheet.create({
   bottomButtonContainer: {
     flexDirection: "row",
     justifyContent: "space-around",
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
   },
 });
 export default CurrentWorkoutScreen;
